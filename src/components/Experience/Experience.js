0242f8f2c7e89c0ec0e124ac53cc3623bd6f848e
DhirajsGithub <borsedhiraj123@gmail.com>
2024-11-15 00:12:01 +0530

    updated experience ui + update resume

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -1,97 +1,11 @@
-import React from 'react';
-import Particle from '../Particle';
-import {Container} from 'react-bootstrap';
+import React from "react";
+import Particle from "../Particle";
+import { Container } from "react-bootstrap";
+import { FaCalendarAlt, FaMapMarkerAlt, FaBriefcase } from "react-icons/fa";
+import { GoLocation } from "react-icons/go";
+import experiences from "../../data/experiences";
 
 const ExperienceTimeline = () => {
-  const experiences = [
-    {
-      year: 2024,
-      title: 'Intern at Pathbeat',
-      location: 'Bengaluru, Karnataka',
-      position: 'React Native Developer',
-      duration: 'June 2024 - Current',
-      responsibilities: [
-        {
-          title: 'Product Development and Engineering',
-          description: 'Engineered the app from the ground up using Redux, React Query, and React Hooks. Improved performance with lazy loading, memoization, and code splitting. Worked closely with designers and backend developers to ship the product.',
-        },
-        {
-          title: 'API Development and Testing',
-          description: 'Integrated and consumed REST APIs, including Google Auth. Designed and tested corner and edge cases to ensure smooth operation in all scenarios.',
-        },
-        {
-          title: 'Advanced Feature Integration',
-          description: 'Integrated third-party libraries and native modules. Developed custom animations, transitions, deep linking, Razorpay for payments, and caching to enhance user experience.',
-        },
-      ],
-    },
-    {
-      year: 2024,
-      title: 'Intern at Trainity',
-      location: 'Bengaluru, Karnataka',
-      position: 'Full Stack Web Developer',
-      duration: 'Feb 2024 - April 2024',
-      responsibilities: [
-        {
-          title: 'Backend Development',
-          description: 'Developed and maintained REST APIs using Express.js and MongoDB. Implemented authentication and authorization with JWT and OAuth. Optimized database queries and backend performance, and ensured scalability and maintainability of codebase.',
-        },
-        {
-          title: 'Frontend Development',
-          description: 'Converted Figma designs into responsive React components using Material-UI and integrated backend APIs for seamless interaction.',
-        },
-      ],
-    },
-    {
-      year: 2023,
-      title: 'Intern at Acumensa Technologies',
-      location: 'Bengaluru, Karnataka',
-      position: 'Frontend Developer',
-      duration: 'April 2023 - August 2023',
-      responsibilities: [
-        {
-          title: 'Frontend Development',
-          description: 'Developed complex React components for a B2C healthcare web solution, optimizing UI/UX for end users.',
-        },
-        {
-          title: 'API Integration',
-          description: 'Integrated APIs with efficient error handling and data management.',
-        },
-        {
-          title: 'Collaboration',
-          description: 'Worked with Delivery Lead, Backend, and QA teams, applying Agile methodologies for timely feature delivery.',
-        },
-      ],
-    },
-    {
-      year: 2022,
-      title: 'Tech Fest Lead at IIT Dharwad (Parsec)',
-      location: 'IIT Dharwad',
-      position: 'Web Development Team Lead',
-      duration: 'Jan 2022 - March 2022',
-      responsibilities: [
-        {
-          title: 'Tech Fest Lead',
-          description: 'Led a team of 6 developers in creating a responsive MERN stack website for the college tech fest, ensuring project success.',
-        },
-      ],
-    },
-    {
-      year: 2022,
-      title: 'Inter IIT Tech Meet',
-      location: 'IIT Kanpur',
-      position: 'Bronze Medalist',
-      duration: 'Feb 2022 - April 2022',
-      responsibilities: [
-        {
-          title: 'Full Stack Developer',
-          description: 'Built a sustainable and well-connected high-speed distribution network for e-commerce brands offering same-day delivery solutions. Played a key role in unlocking growth and simplifying scalability for these brands.',
-        },
-      ],
-    },
-  ];
-  
-
   return (
     <Container fluid className="project-section">
       <Particle />
@@ -101,124 +15,237 @@ const ExperienceTimeline = () => {
           {experiences.map((exp, index) => (
             <div key={index} className="timeline-item">
               <div className="timeline-item-content">
-                <span className="tag" style={{ background: '#be50f4' }}>
-                  {exp.year}
-                </span>
                 <h3>{exp.title}</h3>
-                <h5>{exp.position} | {exp.location}</h5>
-                <p><em>{exp.duration}</em></p>
-  
+                <div className="timeline-item-header">
+                  <div className="timeline-item-date">
+                    <FaCalendarAlt />
+                    <span>{exp.year}</span>
+                  </div>
+                  <div className="timeline-item-location">
+                    <GoLocation />
+                    <span>{exp.location}</span>
+                  </div>
+                </div>
+                <div className="timeline-item-details">
+                  <div className="timeline-item-position">
+                    <FaBriefcase />
+                    <span>{exp.position}</span>
+                  </div>
+                  <div className="timeline-item-duration">
+                    <FaCalendarAlt />
+                    <span>{exp.duration}</span>
+                  </div>
+                </div>
                 {exp.responsibilities && (
-                  <ul>
+                  <ul className="timeline-item-responsibilities">
                     {exp.responsibilities.map((responsibility, idx) => (
                       <li key={idx}>
-                        <strong>{responsibility.title}:</strong> {responsibility.description}
+                        <strong>{responsibility.title}:</strong>{" "}
+                        {responsibility.description}
                       </li>
                     ))}
                   </ul>
                 )}
-  
-                <span className="circle" />
               </div>
+              <div className="timeline-item-circle" />
+              <div className="timeline-item-line" />
             </div>
           ))}
         </div>
-  
-        <style jsx>{`
+      </div>
+
+      <style jsx>{`
+        .timeline-container {
+          font-family: "Poppins", sans-serif;
+          max-width: 900px;
+          margin: 0 auto;
+          padding: 40px 20px;
+          position: relative;
+        }
+        .timeline-header {
+          text-align: center;
+          color: #be50f4;
+          font-size: 32px;
+          font-weight: 700;
+        }
+        .timeline {
+          position: relative;
+          padding: 40px 0;
+          margin-top: 40px;
+        }
+        .timeline::before {
+          content: "";
+          background: #be50f4;
+          width: 4px;
+          height: 100%;
+          position: absolute;
+          left: 50%;
+          transform: translateX(-50%);
+        }
+        .timeline-item {
+          display: flex;
+          justify-content: flex-end;
+          left: 10%;
+          position: relative;
+          margin-bottom: 30px;
+        }
+        .timeline-item:nth-child(even) {
+          justify-content: flex-start;
+          padding-right: 0;
+          left: -10%;
+          position: relative;
+        }
+        .timeline-item-content {
+          background-color: #fff;
+          border-radius: 8px;
+          box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+          padding: 20px;
+          position: relative;
+          width: 500px;
+          max-width: 100%;
+          z-index: 1;
+        }
+        .timeline-item-header {
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          margin-bottom: 10px;
+        }
+        .timeline-item-date,
+        .timeline-item-location {
+          display: flex;
+          align-items: center;
+          font-size: 14px;
+          color: #666;
+        }
+        .timeline-item-date svg,
+        .timeline-item-location svg {
+          margin-right: 5px;
+        }
+        h3 {
+          font-size: 22px;
+          font-weight: 700;
+          margin-bottom: 10px;
+          color: #333;
+        }
+        .timeline-item-details {
+          display: flex;
+          justify-content: space-between;
+          font-size: 14px;
+          color: #666;
+          margin-bottom: 10px;
+        }
+        .timeline-item-position,
+        .timeline-item-duration {
+          display: flex;
+          align-items: center;
+        }
+        .timeline-item-position svg,
+        .timeline-item-duration svg {
+          margin-right: 5px;
+        }
+        .timeline-item-responsibilities {
+          list-style-type: disc;
+          text-align: left;
+          font-size: 16px;
+          color: #555;
+        }
+        .timeline-item-circle {
+          background-color: #be50f4;
+          width: 20px;
+          height: 20px;
+          border-radius: 50%;
+          position: absolute;
+          top: 22px;
+          z-index: 2;
+        }
+        .timeline-item:nth-child(even) .timeline-item-circle {
+          right: 43%;
+        }
+        .timeline-item:nth-child(odd) .timeline-item-circle {
+          left: 43%;
+        }
+        .timeline-item-line {
+          background-color: #be50f4;
+          width: 10%;
+          height: 4px;
+          position: absolute;
+          top: 30px;
+          z-index: 0;
+        }
+        .timeline-item:nth-child(even) .timeline-item-line {
+          left: 50%;
+        }
+        .timeline-item:nth-child(odd) .timeline-item-line {
+          right: 50%;
+        }
+        @media screen and (max-width: 500px) {
           .timeline-container {
-            font-family: Arial, sans-serif;
-            max-width: 900px;
-            margin: 0 auto;
-            padding: 20px;
-          }
-          .timeline-header {
-            text-align: center;
-            color: #be50f4;
-            margin-bottom: 40px;
-          }
-          .timeline {
-            position: relative;
-            padding: 20px 0;
-          }
-          .timeline::before {
-            content: '';
-            background: #be50f4;
-            width: 2px;
-            height: 100%;
-            position: absolute;
-            left: 50%;
-            transform: translateX(-50%);
-          }
-          .timeline-item {
-            margin-bottom: 40px;
+             padding: 20px 10px;
           }
           .timeline-item-content {
-            position: relative;
-            width: 45%;
-            padding: 20px;
-            border-radius: 5px;
-            background: #fff;
-            box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
-            margin-left: auto;
+            right: 10%;
+            max-width: 86% !important;
           }
-          .timeline-item:nth-child(odd) .timeline-item-content {
-            margin-left: 0;
-            margin-right: auto;
+           .timeline-item-line {
+            left: -4% !important;
           }
-          .tag {
-            color: #fff;
-            font-size: 0.9rem;
-            padding: 5px 10px;
-            border-radius: 3px;
-            display: inline-block;
-            margin-bottom: 10px;
+          .timeline-item-circle{
+            transform: scale(0.8);
+            left: 10% !important;
           }
-          h3, h5 {
-            margin-bottom: 5px;
-            color: #333;
+        }
+        @media screen and (max-width: 767px) {
+          .timeline-item-content {
+            right: 6%;
+            width: 90% !important;
           }
-          h5 {
-            font-weight: normal;
-            color: #666;
+          .timeline::before {
+            left: 20px;
           }
-          ul {
-            list-style-type: disc;
-            margin-left: 20px;
+
+          .timeline-item {
+            justify-content: flex-end;
           }
-          li {
-            margin-bottom: 10px;
-            font-size: 0.9rem;
+          .timeline-item:nth-child(even) {
+            left: 1.5% !important;
+            justify-content: flex-end;
           }
-          .circle {
-            background: #be50f4;
-            width: 20px;
-            height: 20px;
-            border-radius: 50%;
-            position: absolute;
-            top: 20px;
-            right: -10px;
+          
+          .timeline-item-line, .timeline-item:nth-child(even) .timeline-item-line {
+            left: 1.8% !important;
           }
-          .timeline-item:nth-child(even) .circle {
-            right: auto;
-            left: -10px !important;
+          .timeline-item-circle{
+            transform: scale(0.8);
+            left: 5% !important;
           }
-          @media screen and (max-width: 600px) {
-            .timeline::before {
-              left: 20px;
-            }
-            .timeline-item-content {
-              width: calc(100% - 40px);
-              margin-left: 40px !important;
-            }
-            .timeline-item:nth-child(odd) .timeline-item-content {
-              margin-right: 0;
-            }
+        }
+        @media screen and (max-width: 1050px) {
+          .timeline-item-content {
+            width: 50%;
           }
-        `}</style>
-      </div>
+          .timeline-item {
+            left: 1.5%;
+          }
+          .timeline-item:nth-child(even) {
+            left: -1.5%;
+          }
+          .timeline-item:nth-child(even) .timeline-item-line {
+            left: 41.5%;
+          }
+          .timeline-item:nth-child(odd) .timeline-item-line {
+            right: 41.5%;
+          }
+          .timeline-item:nth-child(even) .timeline-item-circle {
+            right: 50.5%;
+          }
+          .timeline-item:nth-child(odd) .timeline-item-circle {
+            left: 50.5%;
+          }
+        }
+      `}</style>
     </Container>
   );
-  
 };
 
 export default ExperienceTimeline;
---
+++ src/data/experiences.js
@@ -0,0 +1,89 @@
+const experiences = [
+    {
+      year: 2024,
+      title: 'Intern at Pathbeat',
+      location: 'Bengaluru, Karnataka',
+      position: 'React Native Developer',
+      duration: 'June 2024 - Current',
+      responsibilities: [
+        {
+          title: 'Product Development and Engineering',
+          description: 'Engineered the app from the ground up using Redux, React Query, and React Hooks. Improved performance with lazy loading, memoization, and code splitting. Worked closely with designers and backend developers to ship the product.',
+        },
+        {
+          title: 'API Development and Testing',
+          description: 'Integrated and consumed REST APIs, including Google Auth. Designed and tested corner and edge cases to ensure smooth operation in all scenarios.',
+        },
+        {
+          title: 'Advanced Feature Integration',
+          description: 'Integrated third-party libraries and native modules. Developed custom animations, transitions, deep linking, Razorpay for payments, and caching to enhance user experience.',
+        },
+      ],
+    },
+    {
+      year: 2024,
+      title: 'Intern at Trainity',
+      location: 'Bengaluru, Karnataka',
+      position: 'Full Stack Web Developer',
+      duration: 'Feb 2024 - April 2024',
+      responsibilities: [
+        {
+          title: 'Backend Development',
+          description: 'Developed and maintained REST APIs using Express.js and MongoDB. Implemented authentication and authorization with JWT and OAuth. Optimized database queries and backend performance, and ensured scalability and maintainability of codebase.',
+        },
+        {
+          title: 'Frontend Development',
+          description: 'Converted Figma designs into responsive React components using Material-UI and integrated backend APIs for seamless interaction.',
+        },
+      ],
+    },
+    {
+      year: 2023,
+      title: 'Intern at Acumensa Technologies',
+      location: 'Bengaluru, Karnataka',
+      position: 'Frontend Developer',
+      duration: 'April 2023 - August 2023',
+      responsibilities: [
+        {
+          title: 'Frontend Development',
+          description: 'Developed complex React components for a B2C healthcare web solution, optimizing UI/UX for end users.',
+        },
+        {
+          title: 'API Integration',
+          description: 'Integrated APIs with efficient error handling and data management.',
+        },
+        {
+          title: 'Collaboration',
+          description: 'Worked with Delivery Lead, Backend, and QA teams, applying Agile methodologies for timely feature delivery.',
+        },
+      ],
+    },
+    {
+      year: 2022,
+      title: 'Tech Fest Lead at IIT Dharwad (Parsec)',
+      location: 'IIT Dharwad',
+      position: 'Web Development Team Lead',
+      duration: 'Jan 2022 - March 2022',
+      responsibilities: [
+        {
+          title: 'Tech Fest Lead',
+          description: 'Led a team of 6 developers in creating a responsive MERN stack website for the college tech fest, ensuring project success.',
+        },
+      ],
+    },
+    {
+      year: 2022,
+      title: 'Inter IIT Tech Meet',
+      location: 'IIT Kanpur',
+      position: 'Bronze Medalist',
+      duration: 'Feb 2022 - April 2022',
+      responsibilities: [
+        {
+          title: 'Full Stack Developer',
+          description: 'Built a sustainable and well-connected high-speed distribution network for e-commerce brands offering same-day delivery solutions. Played a key role in unlocking growth and simplifying scalability for these brands.',
+        },
+      ],
+    },
+  ];
+
+  export default experiences
